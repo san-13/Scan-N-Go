212e18219672ba6c8e991cce856cbfb919143bf0
Sandeep <sandeepvermaslg77@gmail.com>
2022-01-29 23:40:37 +0530

    Nav Drawer and top app bar

--- app/src/main/java/com/sv/scanngo/MainActivity.kt
+++ app/src/main/java/com/sv/scanngo/MainActivity.kt
@@ -5,17 +5,27 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.sv.scanngo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    lateinit var toggle:ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val tokenFile:SharedPreferences=getSharedPreferences("tokenfile", Context.MODE_PRIVATE)
         val default = ""
         val token=tokenFile.getString("token",default)
         if(token==""){
             startActivity(Intent(this,Signup::class.java))
         }
+        binding.topAppBar.setNavigationOnClickListener{
+            binding.drawerLayout.open()
+        }
+
+
     }
 }
